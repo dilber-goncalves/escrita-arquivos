76e1681e24e21abece948b0975eca5efb5d00ad1
Dilber Goncalves <dilberkeybd@gmail.com>
2024-05-04 14:44:15 -0300

    04-05

---
+++ src/bancodedados.js
@@ -0,0 +1,13 @@
+
+
+const produtos = [
+    {id: 1, nome:"TV Samsung", valor: 129900},
+    {id: 2, nome: "Notebook DELL", valor: 399990},
+    {id: 3, nome: "Mouse Mx", valor: 23000},
+    {id: 4, nome: "Teclado K8", valor: 50000},
+    {id: 5, nome: "Carregador Portatil", valor: 4590},
+    {id: 6, nome: "WebCam", valor: 80000},
+    {id: 7, nome: "Monitor LG", valor: 129900}
+];
+
+module.exports = produtos;
